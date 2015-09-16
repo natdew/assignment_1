bc9fa18529a25c5d41578e3ec459722e307009b4
natdew <natashadewitz@gmail.com>
2015-09-16 15:34:20 -0400

    submit hw 1

--- sketch.js
+++ sketch.js
@@ -1,22 +1,58 @@
 function setup() {
 	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(800, 500);
 }
 
 function draw() {
 	// clear the background - skyblue
-  background(180, 200, 255);
+  background(150, 200, 255);
 
 	// set a fill color white
-	fill(255, 255, 255);
+	fill(255, 200, 90);
 	// draw ground
 	noStroke();
-	rect(0, 300, 640, 100);
+	rect(0, 300, 900, 10);
 
-	// set a stroke color
 
+	fill(10, 240, 255);
+	// rectangle
+	rect(0, 300, 3000, 200);
+	// set a fill color
+
+	fill(255, 255, 255);
 	// draw a circle
-	ellipse(320, 280, 100, 100);
-	ellipse(320, 200, 80, 80);
-	ellipse(320, 140, 60, 60);
+	ellipse(200, 90, 250, 90);
+
+	fill(255, 255, 255);
+	// draw a circle
+	ellipse(600, 100, 200, 50);
+
+
+	fill(240, 210, 200);
+	// rectangle
+	rect(330, 200, 200, 100);
+	// set a fill color
+
+
+	fill(150, 200, 255);
+	// draw a circle
+	ellipse(430, 200, 200, 100);
+
+
+	// triangle
+	fill(205, 255, 205);
+	noStroke();
+	triangle(420, 51, 420, 180, 341, 180);
+
+	// triangle
+	fill(205, 200, 0);
+	noStroke();
+	triangle(420, 50, 420, 180, 518, 180);
+
+
+	fill(200, 210, 200);
+	// rectangle
+	rect(420, 50, 03, 200);
+	// set a fill color
+
 }
