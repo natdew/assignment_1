0276d3238458b88f50675aff9f2d47046131554e
natdew <natashadewitz@gmail.com>
2015-10-22 14:27:11 -0400

    done

--- sketch.js
+++ sketch.js
@@ -5,54 +5,32 @@ function setup() {
 
 function draw() {
 	// clear the background - skyblue
-  background(150, 200, 255);
+  background(32, 43, 97);
 
-	// set a fill color white
-	fill(255, 200, 90);
-	// draw ground
+
+	fill(45, 48, 55);
 	noStroke();
-	rect(0, 300, 900, 10);
+	rect(0, 340, 900, 200);
+
+fill(171, 176, 190);
+triangle(590, 280, 550, 280, 570, 100);
+
+fill(255, 255, 255);
+ellipse(565, 200, 10, 15);
+ellipse(575, 200, 10, 15);
+ellipse(200, 100, 100, 100);
 
 
-	fill(10, 240, 255);
-	// rectangle
-	rect(0, 300, 3000, 200);
-	// set a fill color
+fill(0, 0, 0);
+ellipse(565, 200, 5, 5);
+ellipse(575, 200, 5, 5);
+rect(550, 280, 40, 90);
 
-	fill(255, 255, 255);
-	// draw a circle
-	ellipse(200, 90, 250, 90);
+fill(32, 43, 97);
+ellipse(230, 100, 100, 100);
 
-	fill(255, 255, 255);
-	// draw a circle
-	ellipse(600, 100, 200, 50);
-
-
-	fill(240, 210, 200);
-	// rectangle
-	rect(330, 200, 200, 100);
-	// set a fill color
-
-
-	fill(150, 200, 255);
-	// draw a circle
-	ellipse(430, 200, 200, 100);
-
-
-	// triangle
-	fill(205, 255, 205);
-	noStroke();
-	triangle(420, 51, 420, 180, 341, 180);
-
-	// triangle
-	fill(205, 200, 0);
-	noStroke();
-	triangle(420, 50, 420, 180, 518, 180);
-
-
-	fill(200, 210, 200);
-	// rectangle
-	rect(420, 50, 03, 200);
-	// set a fill color
+s = "Mister Knife and the Dark Nights"
+fill(0, 0, 0);
+text(s, 600, 100, 100, 100);
 
 }
